ea44c0079cef63fae4ae68513a166db0de8f5da1
Shu Nagumo <93372677+shu-ngm@users.noreply.github.com>
2023-11-21 03:48:04 +0100

    use globalThis

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 "use client";
 if (!structuredClone) {
-	window.structuredClone = (value) => JSON.parse(JSON.stringify(value));
+	globalThis.structuredClone = (value) => JSON.parse(JSON.stringify(value));
 }
 export { createEstate } from "./functions/createEstate";
 export { type Options as EstateOptions } from "./types";
