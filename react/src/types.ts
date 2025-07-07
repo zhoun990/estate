205b7fff4281b0d1c3081d254ca32922b0fea576
Shu Nagumo <72433619+zhoun990@users.noreply.github.com>
2025-07-07 17:41:51 +0200

    feat: update react and core packages to v0.2.15 and v0.2.57, and simplify type exports in types.ts

--- react/src/types.ts
+++ react/src/types.ts
@@ -1,18 +1,5 @@
-import { RootStateType } from "@e-state/core";
+export { SetterFunction, SetEstates } from "@e-state/core";
 
 export type Getter<T> = () => T;
 export type Setter<T> = (payload: T, forceRenderer?: boolean) => void;
-
-export type SetterFunction<T, RootState> = (
-  currentValue: T,
-  rootState: RootState
-) => ReturnValue<T>;
-type ReturnValue<T> = Promise<T> | T;
-export type SetEstates<RootState extends RootStateType> = {
-  [slice in keyof RootState]: Setter<{
-    [key in keyof RootState[slice]]?:
-      | RootState[slice][key]
-      | SetterFunction<RootState[slice][key], RootState>;
-  }>;
-};
 export type DependencyList = readonly unknown[];
